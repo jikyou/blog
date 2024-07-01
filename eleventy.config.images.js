1cb1fb5b66ddac82d15852f98765a232e28d1e40
ChenZiQiang <ziqiang.chen@outlook.com>
2024-07-02 00:14:14 +0900

    Update close @11ty/eleventy-img to temporary fix vercel's munmap_chunk(): invalid pointer

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -12,6 +12,7 @@ module.exports = eleventyConfig => {
 	// Eleventy Image shortcode
 	// https://www.11ty.dev/docs/plugins/image/
 	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
+		return;
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
 		let formats = ["avif", "webp", "auto"];
